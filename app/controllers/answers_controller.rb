42c4cc85aa4964354a364d19823317ef8812e6ef
vpfaulkner@gmail.com <vpfaulkner@gmail.com>
2014-10-22 11:53:46 -0400

    Added controller actions

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,28 @@
 class AnswersController < ApplicationController
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def index
+
+  end
+
+  def vote
+
+  end
+
+  private
+
+  def set_answer
+
+  end
+
+  def answer_params
+
+  end
+  
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,28 @@
 class QuestionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  private
+
+  def set_question
+
+  end
+
+  def question_params
+
+  end
 end
